f42408653c880482a5860b78b22bb31c91760a1f
tgstyle <{ID}+{username}@users.noreply.github.com>
2020-12-27 23:01:57 -0700

    Revert TE replacement

--- src/main/java/mctmods/smelteryio/registry/RegistryTE.java
+++ src/main/java/mctmods/smelteryio/registry/RegistryTE.java
@@ -5,16 +5,17 @@ import mctmods.smelteryio.tileentity.TileEntityAD;
 import mctmods.smelteryio.tileentity.TileEntityCM;
 import mctmods.smelteryio.tileentity.TileEntityFC;
 import mctmods.smelteryio.tileentity.TileEntitySI;
-import net.minecraft.util.ResourceLocation;
+
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class RegistryTE {
 
+	@SuppressWarnings("deprecation")
 	public static void registerTileEntities() {
-		GameRegistry.registerTileEntity(TileEntityFC.class, new ResourceLocation(SmelteryIO.MODID, "TileEntityFC"));
-		GameRegistry.registerTileEntity(TileEntityCM.class, new ResourceLocation(SmelteryIO.MODID, "TileEntityCM"));
-		GameRegistry.registerTileEntity(TileEntitySI.class, new ResourceLocation(SmelteryIO.MODID, "TileEntitySI"));
-		GameRegistry.registerTileEntity(TileEntityAD.class, new ResourceLocation(SmelteryIO.MODID, "TileEntityAD"));
+		GameRegistry.registerTileEntity(TileEntityFC.class, SmelteryIO.MODID + "TileEntityFC");
+		GameRegistry.registerTileEntity(TileEntityCM.class, SmelteryIO.MODID + "TileEntityCM");
+		GameRegistry.registerTileEntity(TileEntitySI.class, SmelteryIO.MODID + "TileEntitySI");
+		GameRegistry.registerTileEntity(TileEntityAD.class, SmelteryIO.MODID + "TileEntityAD");
 	}
 
 }
