1d3122a3d0754ab32b9ebf7313c458ff41717429
Tim <tim@tgobb.com>
2026-02-12 14:18:04 -0700

    Build updates

--- src/main/java/mctmods/smelteryio/mixin/MixinNetworkRegistry.java
+++ src/main/java/mctmods/smelteryio/mixin/MixinNetworkRegistry.java
@@ -1,5 +1,6 @@
 package mctmods.smelteryio.mixin;
 
+import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.relauncher.Side;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,9 +12,20 @@ public class MixinNetworkRegistry {
 
     // NOT FOR DISTRIBUTION! build.gradle prevents this file from being allowed in the jar.
 
+    // Only active in deobf/dev environment (helps prevent accidental usage if accidentally distributed)
     @Redirect(method = "newChannel(Ljava/lang/String;[Lio/netty/channel/ChannelHandler;)Ljava/util/EnumMap;", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/relauncher/Side;values()[Lnet/minecraftforge/fml/relauncher/Side;"), remap = false)
-    private Side[] redirectNewChannel1() { return new Side[]{Side.CLIENT, Side.SERVER}; }
+    private Side[] redirectNewChannel1() {
+        Object isDeobf = Launch.blackboard.get("fml.deobfuscatedEnvironment");
+        boolean applyFix = (isDeobf instanceof Boolean && (Boolean) isDeobf);
+        if (applyFix) { return new Side[]{Side.CLIENT, Side.SERVER}; }
+        return Side.values();
+    }
 
     @Redirect(method = "newChannel(Lnet/minecraftforge/fml/common/ModContainer;Ljava/lang/String;[Lio/netty/channel/ChannelHandler;)Ljava/util/EnumMap;", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fml/relauncher/Side;values()[Lnet/minecraftforge/fml/relauncher/Side;"), remap = false)
-    private Side[] redirectNewChannel2() { return new Side[]{Side.CLIENT, Side.SERVER}; }
+    private Side[] redirectNewChannel2() {
+        Object isDeobf = Launch.blackboard.get("fml.deobfuscatedEnvironment");
+        boolean applyFix = (isDeobf instanceof Boolean && (Boolean) isDeobf);
+        if (applyFix) { return new Side[]{Side.CLIENT, Side.SERVER}; }
+        return Side.values();
+    }
 }
